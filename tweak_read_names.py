0ee1fa332d9f5876fce04f73d02c4aec688f2608
Andrew Tedder <54320061+DrATedder@users.noreply.github.com>
2026-06-19 10:16:06 +0100

    Update tweak_read_names.py

--- tweak_read_names.py
+++ tweak_read_names.py
@@ -4,7 +4,7 @@ import os
 directory = '/storage02/or-microbio/meta_sim_test_25'
 
 for sample in glob.glob(directory + '/aUoBsim*.fastq'):
-    with open('{0}/{1}_RNT_{2}'.format(directory, os.path.basename(sample).split('_')[0]), os.path.basename(sample.split('_')[1])), 'w') as f_out:
+    with open('{0}/{1}_RNT_{2}'.format(directory, os.path.basename(sample).split('_')[0], os.path.basename(sample.split('_')[3])), 'w') as f_out:
         with open(sample, 'r') as f_in:
             for line in f_in:
                 if line.startswith('@'):
